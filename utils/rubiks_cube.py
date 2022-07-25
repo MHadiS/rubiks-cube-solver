780dc83608cf83d1b88d2e7f24fc342cb601b2bd
Mohammad Hadi Shakibazad <hadishakib6561@gmail.com>
2022-07-25 20:10:29 +0430

    FEAT: add START_STATE constance to settings.py

--- utils/rubiks_cube.py
+++ utils/rubiks_cube.py
@@ -2,5 +2,5 @@ import settings as s
 
 
 class RubiksCube:
-    def __init__(self, state: dict = s.START_STATE) -> None:
+    def __init__(self, state: list = s.START_STATE) -> None:
         self.state = state
