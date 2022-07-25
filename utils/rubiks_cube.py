5643b432e65b828c52846edb46ea4c59f1570fd8
Mohammad Hadi Shakibazad <hadishakib6561@gmail.com>
2022-07-25 23:36:44 +0430

    FEAT: add __up method for U movement of rubiks cube

--- utils/rubiks_cube.py
+++ utils/rubiks_cube.py
@@ -9,3 +9,20 @@ class RubiksCube:
         if self.state == s.START_STATE:
             return True
         return False
+
+    def __up(self, inverse: bool = False):
+        blue = state['b'][0]
+        green = state['g'][0]
+        yellow = state['y'][0]
+        white = state['w'][0]
+        new_value = None
+
+        if inverse:
+            new_values = (green, yellow, blue, white)
+        else:
+            new_value = (blue, white, green, yellow)
+
+        state = self.state.copy()
+        (state['w'][0], state['g'][0], state['y'][0], state['b'][0]) = new_values
+
+        return RubiksCube(state)
