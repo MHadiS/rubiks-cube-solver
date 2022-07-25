1f6d3e1529dc2d0fc88e7fbff725393ec7a7dcc6
Mohammad Hadi Shakibazad <hadishakib6561@gmail.com>
2022-07-25 20:16:05 +0430

    FEAT: add solved method to RubiksCube class

--- utils/rubiks_cube.py
+++ utils/rubiks_cube.py
@@ -4,3 +4,8 @@ import settings as s
 class RubiksCube:
     def __init__(self, state: list = s.START_STATE) -> None:
         self.state = state
+    
+    def solved(self):
+        if self.state == s.START_STATE:
+            return True
+        return False
